fe7b3c09ef2cc5a60f2041513c05a9ec5ff96588
Jakob Jarosch <dev@jakobjarosch.de>
2021-03-21 00:36:37 +0100

    Wait a few seconds in case code is not yet loaded

--- libs/microfrontend-react/src/createRemoteComponent.tsx
+++ libs/microfrontend-react/src/createRemoteComponent.tsx
@@ -6,15 +6,28 @@ export const createRemoteComponent: <T>(
   component: string | undefined
 ) => React.FC<T> = (microfrontendName, module, component) => {
   const Component = React.lazy(async () => {
-    // @ts-ignore
-    await __webpack_init_sharing__('default');
-    // @ts-ignore
-    const defaultScope = __webpack_share_scopes__.default;
-    const container = (window as any)[`microfrontend_${microfrontendName}`];
-    await container.init(defaultScope);
-    const factory = await container.get(module);
-    const jsModule = factory();
-    return { default: component ? jsModule[component] : jsModule };
+    try {
+      // @ts-ignore
+      await __webpack_init_sharing__('default');
+      // @ts-ignore
+      const defaultScope = __webpack_share_scopes__.default;
+      let container;
+      for (let i = 0; i < 50; i++) {
+        container = (window as any)[`microfrontend_${microfrontendName}`];
+        if (!container) {
+          await sleep(100);
+        } else {
+          break;
+        }
+      }
+      await container.init(defaultScope);
+      const factory = await container.get(module);
+      const jsModule = factory();
+      return { default: component ? jsModule[component] : jsModule };
+    } catch (e) {
+      console.error(e);
+      return { default: () => <div style={{ color: 'red' }}>Failed to load Component!</div> };
+    }
   });
 
   return (props) => (
@@ -25,3 +38,10 @@ export const createRemoteComponent: <T>(
     </React.Suspense>
   );
 };
+
+const sleep = (timeout: number) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(undefined);
+    }, timeout);
+  });
